64c77746db637b624a76d4e3b5919d23c3d7e52f
Anubhav Thapa <magaranub@gmail.com>
2021-03-10 16:04:09 +0545

    Resnet model

--- app.py
+++ app.py
@@ -3,30 +3,35 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing import image
 from tensorflow.python.keras.layers.core import Dropout
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.applications.resnet50 import preprocess_input, ResNet50
 from glob import glob
 from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 IMAGE_SIZE = [224, 224]
 train_path = './train'
 valid_path = './test'
 
-vgg = VGG16(input_shape=IMAGE_SIZE+[3], weights='imagenet', include_top=False)
+res = ResNet50(input_shape=(224, 224, 3), include_top=False)
 
-for layer in vgg.layers:
+for layer in res.layers:
     layer.trainable = False
 
 folders = glob('train/*')
 
-x = Flatten()(vgg.output)
+x = Flatten()(res.output)
+x = Dense(units=2, activation='sigmoid', name='predictions')(x)
 
-prediction = Dense(len(folders), activation='softmax')(x)
+model = Model(res.input, x)
 
-model = Model(inputs=vgg.input, outputs=prediction)
-
-model.compile(optimizer='adam', loss='binary_crossentropy',
+model.compile(optimizer='adam', loss='categorical_crossentropy',
               metrics=['accuracy'])
 
+es = EarlyStopping(monitor="val_accuracy",
+                   min_delta=0.01, patience=3, verbose=1)
+mc = ModelCheckpoint(filepath="bestmodel.h5",
+                     monitor="val_accuracy", verbose=1, save_best_only=True)
 
 train_datagen = image.ImageDataGenerator(
     rescale=1/255, horizontal_flip=True, zoom_range=0.2, shear_range=0.2)
@@ -37,7 +42,8 @@ test_datagen = image.ImageDataGenerator(rescale=1/255)
 test_data = test_datagen.flow_from_directory(
     './test', target_size=(244, 244), batch_size=32, class_mode='binary')
 
-model.fit_generator(train_data, validation_data=test_data, epochs=25)
+model.fit_generator(train_data, steps_per_epoch=10, epochs=30,
+                    validation_data=test_data, validation_steps=16, callbacks=[es, mc])
 
 
 def predict_img(img_path):
