a070044eec1e1496d865f54e58a5e1795e543c01
Anubhav Thapa <magaranub@gmail.com>
2021-03-10 07:04:01 +0545

    VGG16 model

--- app.py
+++ app.py
@@ -3,6 +3,30 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing import image
 from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from glob import glob
+from tensorflow.keras.models import Model
+
+IMAGE_SIZE = [224, 224]
+train_path = './train'
+valid_path = './test'
+
+vgg = VGG16(input_shape=IMAGE_SIZE+[3], weights='imagenet', include_top=False)
+
+for layer in vgg.layers:
+    layer.trainable = False
+
+folders = glob('train/*')
+
+x = Flatten()(vgg.output)
+
+prediction = Dense(len(folders), activation='softmax')(x)
+
+model = Model(inputs=vgg.input, outputs=prediction)
+
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy'])
+
 
 train_datagen = image.ImageDataGenerator(
     rescale=1/255, horizontal_flip=True, zoom_range=0.2, shear_range=0.2)
@@ -13,21 +37,7 @@ test_datagen = image.ImageDataGenerator(rescale=1/255)
 test_data = test_datagen.flow_from_directory(
     './test', target_size=(256, 256), batch_size=32, class_mode='binary')
 
-model = Sequential()
-model.add(Conv2D(filters=32, kernel_size=3,
-          activation='relu', input_shape=[256, 256, 3]))
-model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(rate=0.25))
-model.add(Conv2D(filters=32, kernel_size=3, activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(rate=0.25))
-model.add(Flatten())
-model.add(Dense(units=64, activation='relu'))
-model.add(Dense(units=1, activation='sigmoid'))
-model.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(train_data, validation_data=test_data, epochs=25)
+model.fit_generator(train_data, validation_data=test_data, epochs=25)
 
 
 def predict_img(img_path):
